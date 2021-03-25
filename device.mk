3b5091c7572fc152b1017369836abc0cf1e7437e
Edwin Wong <edwinwong@google.com>
2021-03-25 14:36:24 -0700

    mido: Update clearkey plugin to drm HAL v1.4
    Test: atest
      atest CtsMediaTestCases:android.media.cts.MediaDrmClearkeyTest#testGetPlaybackComponent
    
    Bug: 182525516
    Change-Id: I406b7d3c2921d4698535421894c6eb3785eb1c0d
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>
    Signed-off-by: Jrchintu <Harrypetersun@outlook.com>

--- device.mk
+++ device.mk
@@ -206,13 +206,11 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.1 \
-    android.hardware.drm@1.1.vendor \
-    android.hardware.drm@1.2 \
-    android.hardware.drm@1.2.vendor \
-    android.hardware.drm@1.3 \
-    android.hardware.drm@1.3.vendor \
-    android.hardware.drm@1.3-service.clearkey
+    android.hardware.drm@1.4 \
+    android.hardware.drm@1.4.vendor \
+    android.hardware.drm@1.0-impl:64 \
+    android.hardware.drm@1.0-service-lazy \
+    android.hardware.drm@1.4-service.clearkey
 
 # Fingerprint
 PRODUCT_PACKAGES += \
