13a4a571d20613f90f0907bf958941680fb02147
Jrchintu <Harrypetersun@outlook.com>
2021-10-08 13:48:18 +0530

    Add minimal sepolicy but dont use it for now
    Signed-off-by: Jrchintu <Harrypetersun@outlook.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -176,6 +176,7 @@ include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 #SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 #SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
+#BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/minimal
 
 # Treble
 PRODUCT_FULL_TREBLE_OVERRIDE := true
