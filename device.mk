8f8de45c52f61c5ce081d594bdbba2c3fb6b0ec5
Jrchintu <Harrypetersun@outlook.com>
2021-10-15 11:15:00 +0530

    Revert "Drop telephony-ext bootjar"
    This reverts commit afdc526356c8e6fd21c57fe9adb00fc624feab33.

--- device.mk
+++ device.mk
@@ -409,6 +409,9 @@ PRODUCT_PACKAGES += \
     telephony-ext \
     libxml2
 
+PRODUCT_BOOT_JARS += \
+    telephony-ext
+
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl:64 \
