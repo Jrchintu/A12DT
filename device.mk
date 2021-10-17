ed4afa1f7851ac58b5e89b1ea7be3133e6d8fbed
Jrchintu <Harrypetersun@outlook.com>
2021-10-17 16:25:51 +0530

    Update bluetooth audio HIDL to v2.1
    Signed-off-by: Jrchintu <Harrypetersun@outlook.com>

--- device.mk
+++ device.mk
@@ -90,7 +90,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl:32 \
     android.hardware.audio.effect@6.0-impl:32 \
     android.hardware.audio.service \
-    android.hardware.bluetooth.audio@2.0-impl:32 \
+    android.hardware.bluetooth.audio@2.1-impl:32 \
     android.hardware.bluetooth.audio@2.1 \
     android.hardware.bluetooth.a2dp@1.0 \
     android.hardware.audio.effect@7.0 \
